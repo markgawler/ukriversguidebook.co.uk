dbfae02cb8055e24772012e43c325be203404740
Mark Gawler <markg@wler.co.uk>
2012-11-26 16:06:26 +0000

    Add a bit more of the backend configuration.

--- Component/com_ukrgb/admin/sql/install.mysql.utf8.sql
+++ Component/com_ukrgb/admin/sql/install.mysql.utf8.sql
@@ -62,3 +62,9 @@ CREATE TABLE `#__ukrgb_riverguides` (
 `takeout_geo` POINT ,
 `grade` TINYINT NOT NULL
 ) ENGINE = MYISAM CHARACTER SET utf8 COLLATE utf8_general_ci;
+
+CREATE TABLE `#__ukrgb_configuration` (
+`id` INT( 11 ) NOT NULL AUTO_INCREMENT PRIMARY KEY ,
+`config_name` varchar(20),
+`config_value` varchar(20)
+) ENGINE = MYISAM CHARACTER SET utf8 COLLATE utf8_general_ci;
---
+++ Component/com_ukrgb/admin/sql/updates/mysql/0.0.14.sql
@@ -0,0 +1,6 @@
+
+CREATE TABLE `#__ukrgb_configuration` (
+`id` INT( 11 ) NOT NULL AUTO_INCREMENT PRIMARY KEY ,
+`config_name` varchar(20),
+`config_value` varchar(20)
+) ENGINE = MYISAM CHARACTER SET utf8 COLLATE utf8_general_ci;
