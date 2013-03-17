d8f1eb8e280b7e708083a6a2f1d88486263eb655
Mark Gawler <markg@wler.co.uk>
2013-03-17 09:11:10 +0000

    Initial commit of UKRGB Joomla template (modified BEEZ5)

---
+++ Templates/joomla/ukrgb/html/com_content/category/blog_links.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * @version		$Id: blog_links.php,v 1.3 2011/12/02 17:48:01 mrfg Exp $
+ * @package		Joomla.Site
+ * @subpackage	Templates.ukrgb
+ * @copyright	Copyright (C) 2005 - 2009 Open Source Matters, Inc. All rights reserved.
+ * @license		GNU General Public License version 2 or later; see LICENSE.txt
+ */
+
+// no direct access
+defined('_JEXEC') or die;
+$params =& $this->item->params;
+$app = JFactory::getApplication();
+$templateparams =$app->getTemplate(true)->params;
+
+if ($templateparams->get('html5')!=1)
+{
+	require(JPATH_BASE.'/components/com_content/views/category/tmpl/blog_links.php');
+	//evtl. ersetzen durch JPATH_COMPONENT.'/views/...'
+} else {
+JHtml::addIncludePath(JPATH_COMPONENT . '/helpers');
+
+?>
+
+<div class="items-more">
+<h3><?php echo JText::_('COM_CONTENT_MORE_ARTICLES'); ?></h3>
+
+<ol>
+
+<?php
+	foreach ($this->link_items as &$item) :
+?>
+		 <li>
+		  		<a href="<?php echo JRoute::_(ContentHelperRoute::getArticleRoute($item->slug, $item->catid)); ?>">
+			<?php echo $item->title; ?></a>
+		</li>
+<?php endforeach; ?>
+	</ol>
+</div>
+
+<?php } ?>
