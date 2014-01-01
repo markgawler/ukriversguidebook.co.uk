bcf34b05c559545926de5c8a256415de5cecf9ab
Mark Gawler <markg@wler.co.uk>
2014-01-01 20:21:59 +0000

    Fix Plugin onContentAfterSave not bing called

--- Plugins/plg_ukrgbmap/ukrgbmap.php
+++ Plugins/plg_ukrgbmap/ukrgbmap.php
@@ -8,8 +8,8 @@
 
 defined( '_JEXEC' ) or die( 'Restricted access' );
 
-require_once JPATH_SITE . DS . 'components' . DS . 'com_ukrgb' . DS . 'models' . DS . 'maphelper.php';
-require_once JPATH_SITE . DS . 'components' . DS . 'com_ukrgb' . DS . 'models' . DS . 'mappointshelper.php';
+require_once JPATH_SITE . '/components/com_ukrgb/model/map.php';
+require_once JPATH_SITE . '/components/com_ukrgb/model/mappoint.php';
 
 class plgContentUkrgbMap extends JPlugin {
 	/**
@@ -24,46 +24,29 @@ class plgContentUkrgbMap extends JPlugin {
 	{		
 		if (isset($article->id))
 		{
-			$mapid = UkrgbMapHelper::getMapIdforArticle($article->id);
-		
+			$model = new UkrgbModelMap;
+			$mapid = $model->getMapIdforArticle($article->id);
+			
 			if (isset($mapid)){
-					
+				
 				JHtml::_('behavior.framework');
 				JHtml::_('script', 'http://openlayers.org/api/OpenLayers.js');
-				JHtml::_('script', 'components/com_ukrgb/proj4js/lib/proj4js-compressed.js');
-				JHtml::_('script', 'components/com_ukrgb/views/map/js/OpenSpace.js');
-				JHtml::_('script', 'components/com_ukrgb/views/map/js/map-openlayers.js');
-				JHtml::_('stylesheet','components/com_ukrgb/views/map/CSS/map.css');
+				JHtml::_('script', 'libraries/ukrgb/proj4js/proj4js-compressed.js');
+				//JHtml::_('script', 'components/com_ukrgb/view/map/js/OpenSpace.js');
+				JHtml::_('script', 'components/com_ukrgb/view/map/js/map-openlayers.js');
 				
 				
 				$mapDiv = '<div id="map" class="ukrgbmap"></div>
-				<form class="ukrgbmapgr">
-				<label>OS Grid Ref: </label><input type="text" id="GridRef" size="12" readonly>
-				<label>   WGS84 Lat: </label><input type="text" id="Lat" size="8" readonly>
-				<label>Lng: </label><input type="text" id="Lng" size="8" readonly>		
+				<form class="ukrgbmapgr form-inline">
+				<label>OS Grid Ref: </label><input class="input-small uneditable-input" type="text" id="GridRef" readonly>
+				<label>WGS84 Lat: </label><input class="input-mini uneditable-input" type="text" id="Lat"  readonly>
+				<label>Lng: </label><input class="input-mini uneditable-input" type="text" id="Lng" readonly>
 				</form>';
-				
-				/*$user = JFactory::getUser();
-				$canEdit = $user->authorise( "core.edit", "com_content.article." . $article->id );
-				if ($canEdit){
-					$form = '<fieldset>
-						<legend>Map Edit:</legend>
-						<form action="">
-							<input type="button" onclick="submitMapAction()" value="Submit" />
-						</form>
-						</fieldset>';
-					
-					$mapDiv = $mapDiv .$form;
-					JHtml::_('script', 'components/com_ukrgb/views/map/js/map-edit.js');
-				}
-				else 
-				{
-					JHtml::_('script', 'components/com_ukrgb/views/map/js/map.js');
-				}*/
 	
+				
 				$mapData = json_encode(array(
 						'url' => JURI::base() . 'index.php?option=com_ukrgb&tmpl=raw&format=json',
-						'mapdata' => UkrgbMapHelper::getBasicMapData($mapid)));
+						'mapdata' => $model->getMapParameters($mapid)));
 				
 				$document = &JFactory::getDocument();
 				$document->addScriptDeclaration('var params = ' .$mapData.';');
@@ -74,10 +57,10 @@ class plgContentUkrgbMap extends JPlugin {
 		}
 	}
 	
-	public function onContentAfterSave($context, &$article, $isNew)
+	public function onContentAfterSave($context, $article, $isNew)
 	{
-		UkrgbMapPointsHelper::updateMapPoints($article->introtext, $article->id, $article->title);
-				
+		$model = new UkrgbModelMappoint;
+		$model->updateMapPoints($article->introtext, $article->id, $article->title);				
 	}
 }
 ?>
